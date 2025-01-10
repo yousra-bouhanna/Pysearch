7699b556c4db81a49f087be6eb69c5ed4278348b
yousra bouhanna <y_bouhanna@estin.dz>
2025-01-10 21:26:34 +0100

    Read ME and corpora update

--- Corpora.py
+++ Corpora.py
@@ -13,69 +13,79 @@ import Modules.documentFactory as DocumentFactory
 from Modules.searchEngine import SearchEngine
 import os
 
-
 # Scapping des données Reddit 
 reddit = praw.Reddit(client_id='c1P8veYayu3RTOnAeMOREw', client_secret='pBIMxgjrBv3TC2cpkROBFVbbnzSObw', user_agent='Red WebScraping')
-id2doc={}
-id2aut={}
-hot_posts=reddit.subreddit('NLP').hot(limit=20000)
+id2doc = {}
+id2aut = {}
+reddit_id2doc = {}
+reddit_id2aut = {}
+hot_posts = reddit.subreddit('NLP').hot(limit=20000)
 for i, post in enumerate(hot_posts):
     if post.selftext:  # On évite les posts sans texte
-        date = datetime.datetime.fromtimestamp(post.created, tz=datetime.timezone.utc)#Ajouté avec l'aide de copilot pour la résolution d'une erreur de comparaison de dates
+        date = datetime.datetime.fromtimestamp(post.created, tz=datetime.timezone.utc)  # Ajouté avec l'aide de copilot pour la résolution d'une erreur de comparaison de dates
         num_comments = post.num_comments
-        doc = RedditDocument( titre=post.title, auteur=post.author, date=date, url=post.url, texte=post.selftext, nb_com=post.num_comments)
+        doc = RedditDocument(titre=post.title, auteur=post.author, date=date, url=post.url, texte=post.selftext, nb_com=post.num_comments)
         id2doc[i] = doc
+        reddit_id2doc[i] = doc
         author_name = str(post.author)
         if author_name not in id2aut:
             id2aut[author_name] = Author(author_name)
+        if author_name not in reddit_id2aut:
+            reddit_id2aut[author_name] = Author(author_name)
         id2aut[author_name].add(doc)
+        reddit_id2aut[author_name].add(doc)
 
 # Sauvgarde des documents Reddit sous format csv 
-Reddit_data=pd.DataFrame(columns=["id", "titre","auteur","date","url","texte","origin"])
-for i, doc in id2doc.items():
-    Reddit_data.loc[i]=[i, doc.titre, doc.auteur, doc.date, doc.url, doc.texte, doc.type]
+Reddit_data = pd.DataFrame(columns=["id", "titre", "auteur", "date", "url", "texte", "origin"])
+for i, doc in reddit_id2doc.items():
+    Reddit_data.loc[i] = [i, doc.titre, doc.auteur, doc.date, doc.url, doc.texte, doc.type]
 Reddit_data.to_csv(os.path.join("Data", "Reddit_data.csv"), sep="\t", index=False)
 
 # La chaîne de caractère qui contient le texte de tous les documents Reddit
-texte_reddit = " ".join([doc.texte for doc in id2doc.values()])
+texte_reddit = " ".join([doc.texte for doc in reddit_id2doc.values()])
 
 # Création du corpus Reddit
-corpus_reddit = Corpus("Reddit_corpus", id2aut, id2doc, texte_reddit)
+corpus_reddit = Corpus("Reddit_corpus", reddit_id2aut, reddit_id2doc, texte_reddit)
 corpus_reddit.save(os.path.join("Data", "Reddit_corpus.pkl"))
 print(corpus_reddit)
 
 # Scraping des données ArXiv
-query="NLP"
-url="http://export.arxiv.org/api/query?search_query=all:"+query+"&start=0&max_results=5000"
-data=urllib.request.urlopen(url).read()
-data=data.decode()
+query = "NLP"
+url = "http://export.arxiv.org/api/query?search_query=all:" + query + "&start=0&max_results=5000"
+data = urllib.request.urlopen(url).read()
+data = data.decode()
 # Conversion de data qui est sous forme XML en dictionnaire
-dic=xmltodict.parse(data)
-docs=dic["feed"]["entry"]
+dic = xmltodict.parse(data)
+docs = dic["feed"]["entry"]
+arxiv_id2doc = {}
+arxiv_id2aut = {}
 for i, doc in enumerate(docs):
     authors = doc["author"]
     author_names = [author["name"] for author in authors] if isinstance(authors, list) else [authors["name"]]
     date = datetime.datetime.fromisoformat(doc["published"].replace("Z", "+00:00"))
-    doc = ArXivDocument( titre=doc["title"], auteur=author_names, date=date, url=doc["id"], texte=doc["summary"])
+    doc = ArXivDocument(titre=doc["title"], auteur=author_names, date=date, url=doc["id"], texte=doc["summary"])
     for author_name in author_names:
         if author_name not in id2aut:
             id2aut[author_name] = Author(author_name)
+        if author_name not in arxiv_id2aut:
+            arxiv_id2aut[author_name] = Author(author_name)
         id2aut[author_name].add(doc)
+        arxiv_id2aut[author_name].add(doc)
     
+    arxiv_id2doc[max(arxiv_id2doc.keys(), default=0) + 1] = doc
     id2doc[max(id2doc.keys(), default=0) + 1] = doc
 
 # Sauvgarde des documents ArXiv sous format csv
-Arxiv_data=pd.DataFrame(columns=["id", "titre","auteur","date","url","texte","origin"])
-for i, doc in id2doc.items():
-    if isinstance(doc, ArXivDocument): 
-        Arxiv_data.loc[i]=[i, doc.titre, doc.auteur, doc.date, doc.url, doc.texte, doc.type]
+Arxiv_data = pd.DataFrame(columns=["id", "titre", "auteur", "date", "url", "texte", "origin"])
+for i, doc in arxiv_id2doc.items():
+    Arxiv_data.loc[i] = [i, doc.titre, doc.auteur, doc.date, doc.url, doc.texte, doc.type]
 Arxiv_data.to_csv(os.path.join("Data", "Arxiv_data.csv"), sep="\t", index=False)
 
 # La chaîne de caractère qui contient le texte de tous les documents ArXiv
-texte_arxiv = " ".join([doc.texte for doc in id2doc.values() if isinstance(doc, ArXivDocument)])
+texte_arxiv = " ".join([doc.texte for doc in arxiv_id2doc.values()])
 
 # Création du corpus ArXiv
-corpus_arxiv = Corpus("ArXiv_corpus", id2aut, id2doc, texte_arxiv)
+corpus_arxiv = Corpus("ArXiv_corpus", arxiv_id2aut, arxiv_id2doc, texte_arxiv)
 corpus_arxiv.save(os.path.join("Data", "ArXiv_corpus.pkl"))
 print(corpus_arxiv)
 
@@ -84,5 +94,3 @@ texte_global = texte_reddit + " " + texte_arxiv
 corpus_global = Corpus("Global_corpus", id2aut, id2doc, texte_global)
 corpus_global.save(os.path.join("Data", "Global_corpus.pkl"))
 print(corpus_global)
-
-
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Pysearch
+# Pysearch: Moteur de Recherche Personnalisé - Traitement du Langage Naturel (NLP)
+
+## Présentation du projet
+Ce projet a été réalisé dans le cadre du cours de programmation avec Python. Il s'agit d'un moteur de recherche personnalisé axé sur le thème du **Traitement du Langage Naturel (NLP)**. Le projet extrait et analyse des données issues de deux sources principales : **Reddit** et **ArXiv**. L'objectif est de permettre à l'utilisateur d'effectuer des recherches avancées, de comparer les deux corpus, et de visualiser les tendances et les éléments clés sous forme graphique.
+
+## fonctionnalités principales
+- **Recherche Classique**:
+    Recherche par texte libre dans les deux corpus.
+
+- **Recherche Avancée**
+    Filtrage par :
+    - Mots-clés
+    - Date 
+    - Auteur 
+    - Source (Reddit ou ArXiv)
+
+- **Comparaison des Corpus**
+    Génération de nuages de mots :
+    - Mots spécifiques à chaque corpus
+    - Mots en commun entre les deux corpus
+
+- **Analyse Temporelle**
+    Tracé de graphes montrant l'évolution temporelle de la fréquence d'un mot :
+    - Fréquence annuelle
+    - Fréquence mensuelle
+
+## Structure du projet
+
+
+## Technologies utilisées
+- Interface: streamlit
+- Backend: Python
+
+## Prérequis: 
+Toutes les bibliothèques et dépendances nécessaires sont listées dans le fichier requirements.txt, situé à la racine du projet [requirements.txt](requirements.txt)
+
+## Installation:
+1. Clonez le projet :
+    ```bash
+    git clone https://github.com/yousra-bouhanna/Pysearch.git
+    cd Pysearch
+
+2. Créez et activez un environnement virtuel :
+    ```bash
+    python -m venv .venv
+    source .venv/bin/activate  # Pour Unix
+    .\.venv\Scripts\activate   # Pour Windows 
+
+3. Installez les dépendances :
+    ```bash
+    pip install -r requirements.txt
+
+4. Lancez l'application :
+    ```bash
+    streamlit run app.py
+
+
+## Tests:
+Des tests unitaires ont été mis en place pour valider le bon fonctionnement des différentes fonctionnalités du projet. Ils sont disponibles dans le répertoire [.github/Tests](.github/Tests) 
+
+Vous pouvez executer les tests avec la commande:
+    ```bash
+    python -m unittest Tests/test_corpus.py
+
+
+## Les contributeurs:
+
+- [Yousra Bouhanna](https://github.com/yousra-bouhanna)
+- [Mohamed Riad Sahrane](https://github.com/riadshrn)
+
+
+
+
+
+
